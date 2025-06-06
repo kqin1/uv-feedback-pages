3d152db306bc9a9feb43bea659890cc632bceb8b
Kexin Qin <kqin@Kexins-MacBook-Pro-2.local>
2025-06-06 06:38:16 +0200

    ✅ 修复 Doctor 页面空白

--- professional_day7_doctor_feedback.html.py
+++ professional_day7_doctor_feedback.html.py
@@ -42,7 +42,7 @@ html_template = f"""<!DOCTYPE html>
             width: 377px;
             height: 816px;
             margin: 0 auto;
-            background-image: url("https://kqin1.github.io/uv-feedback-pages/Doctor3.png");
+            background-image: url("https://kqin1.github.io/uv-feedback-pages/Doctor7.png");
             background-size: cover;
             background-repeat: no-repeat;
         }}
